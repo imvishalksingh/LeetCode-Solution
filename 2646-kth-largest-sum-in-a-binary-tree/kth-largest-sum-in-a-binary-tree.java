6e5dede420f4b6d2590110769f4f9d74cdbb4a6d
Vishal Singh <161681190+imvishalksingh@users.noreply.github.com>
2024-10-22 13:47:02 +0530

    Time: 41 ms (22.18%) | Memory: 61.2 MB (96.61%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
@@ -15,36 +15,28 @@
  */
 class Solution {
     public long kthLargestLevelSum(TreeNode root, int k) {
-        List<List<Integer>> ls=new ArrayList<>();
+        List<Long> ans=new ArrayList<>();
         Queue<TreeNode> q=new LinkedList<>();
 
-        if(root==null){
-            return -1;
-        }
         q.offer(root);
-        
         while(!q.isEmpty()){
-            int size=q.size();
-            List<Integer> lst=new LinkedList<>();
-            for(int i=0;i<size ;i++){
-                if(q.peek().left!=null) q.offer(q.peek().left);
-                if(q.peek().right!=null) q.offer(q.peek().right);
-                lst.add(q.poll().val);
+            int n=q.size();
+            long sum=0;
+            for(int i=0;i<n;i++){
+                TreeNode node=q.poll();
+                sum+=node.val;
+
+                if(node.left!=null) q.offer(node.left);
+                if(node.right!=null) q.offer(node.right);
             }
-            ls.add(lst);
+            ans.add(sum);
 
         }
-        List<Long> res=new ArrayList<>();
-        long i=0;
-        for(List<Integer> inner : ls){
-            long sum=0;
-                for(int ele:inner){
-                    sum+=ele;
-                }
-                res.add(sum);
-        }
-        res.sort(Collections.reverseOrder());
-       if (k > res.size()) return -1;
-        return res.get(k-1);
+
+        if(k > ans.size()) return -1;
+        ans.sort(Collections.reverseOrder());
+        
+        return ans.get(k-1);
+
     }
 }
